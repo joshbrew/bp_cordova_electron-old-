b8441154f552f8a14ae30e08e89c716b16cf97c6
Bubba Skrimp <brewster.joshua1@gmail.com>
2021-07-11 12:13:38 -0700

    merged pwa, cordova, and standalone

---
+++ www/README.md
@@ -0,0 +1,10 @@
+The package files in this subdirectory are to build a progressive webapp.
+
+To install the PWA: 
+
+From a terminal, cd to this folder (not the external one) and:
+`npm install`
+
+There are errors with the worker with this PWA setup.
+
+You can also just click to open the index.html and run it.
---
+++ www/index.js
@@ -0,0 +1,55 @@
+import 'regenerator-runtime/runtime'
+
+try {
+    if(cordova) {
+        /*
+        * Licensed to the Apache Software Foundation (ASF) under one
+        * or more contributor license agreements.  See the NOTICE file
+        * distributed with this work for additional information
+        * regarding copyright ownership.  The ASF licenses this file
+        * to you under the Apache License, Version 2.0 (the
+        * "License"); you may not use this file except in compliance
+        * with the License.  You may obtain a copy of the License at
+        *
+        * http://www.apache.org/licenses/LICENSE-2.0
+        *
+        * Unless required by applicable law or agreed to in writing,
+        * software distributed under the License is distributed on an
+        * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+        * KIND, either express or implied.  See the License for the
+        * specific language governing permissions and limitations
+        * under the License.
+        */
+    
+        // Wait for the deviceready event before using any of Cordova's device APIs.
+        // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
+    
+        document.addEventListener('deviceready', onDeviceReady, false);
+    
+        function onDeviceReady() {
+            // Cordova is now initialized. Have fun!
+    
+            console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+            document.getElementById('deviceready').classList.add('ready');
+        }
+    
+    } 
+} catch (er) {
+    //no cordova
+    window.PWA = true;
+
+    if (process.env.NODE_ENV === 'development') {
+        console.log('PWA DEV MODE');
+    }
+
+    if (process.env.NODE_ENV === 'production') {    
+        if(!navigator.serial)
+            alert("navigator.serial not found! Enable #enable-experimental-web-platform-features in chrome://flags (search 'experimental')")
+
+        // import * as serviceWorker from './service-worker';
+        const serviceWorker = require('./service-worker');
+        // If you want your app to work offline and load faster, you can change
+        // unregister() to register() below. Note this comes with some pitfalls.
+        serviceWorker.register();
+    }
+}
